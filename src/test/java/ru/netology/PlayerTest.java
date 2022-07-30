163574b8b849b43cd2c83b1b84f61332f58b325c
olga-maev <kurumoch0@gmail.com>
2022-07-30 15:39:12 +0400

    Defect #1,2 Error player.sumGenre and player.mostPlayerByGenre

--- src/test/java/ru/netology/PlayerTest.java
+++ src/test/java/ru/netology/PlayerTest.java
@@ -1,6 +1,8 @@
 package ru.netology;
 
 import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 
 public class PlayerTest {
@@ -18,6 +20,54 @@ public class PlayerTest {
         int actual = player.sumGenre(game.getGenre());
         assertEquals(expected, actual);
     }
+    @Test
+    public void shouldSumGenreIfOneGamePlayDouble() {
+        GameStore store = new GameStore();
+        Game game = store.publishGame("Нетология Баттл Онлайн", "Аркады");
 
-    // другие ваши тесты
+        Player player = new Player("Petya");
+        player.installGame(game);
+        player.play(game, 3);
+        player.play(game, 5);
+
+        int expected = 8;
+        int actual = player.sumGenre(game.getGenre());
+        assertEquals(expected, actual);
+    }
+    @Test
+    public void shouldmostPlayerByGenreOn() {
+        GameStore store = new GameStore();
+        Game game1 = store.publishGame("Нетология Баттл Онлайн", "Аркады");
+        Game game2 = store.publishGame("Нетология Баттл Онлайн", "Шуттер");
+
+        Player player = new Player("Petya");
+        player.installGame(game1);
+        player.installGame(game2);
+        player.play(game1, 3);
+        player.play(game2, 5);
+
+        Game expected = game2;
+        Game actual = player.mostPlayerByGenre(game2.getGenre());
+        assertEquals(expected, actual);
+    }
+    @Test
+    public void shouldmostPlayerByGenreOff() {
+        GameStore store = new GameStore();
+        Game game1 = store.publishGame("Нетология Баттл Онлайн", "Аркады");
+        Game game2 = store.publishGame("Нетология Баттл Онлайн", "Шуттер");
+        Game game3 = store.publishGame("Нетология Баттл Онлайн", "Квест");
+
+        Player player = new Player("Petya");
+        player.installGame(game1);
+        player.installGame(game2);
+        player.play(game1, 3);
+        player.play(game2, 5);
+
+        Game expected = null;
+        Game actual = player.mostPlayerByGenre(game3.getGenre());
+        assertEquals(expected, actual);
+    }
+
+
+//     другие ваши тесты
 }
