10ba7977791a49048a0781794a966136399ad1cf
IvanPliska <94367970+IvanPliska@users.noreply.github.com>
2022-08-01 11:05:33 +0300

    Add 5 Game

--- src/test/java/ru/netology/GameStoreTest.java
+++ src/test/java/ru/netology/GameStoreTest.java
@@ -4,6 +4,12 @@ import static org.junit.jupiter.api.Assertions.*;
 import org.junit.jupiter.api.Test;
 
 public class GameStoreTest {
+    GameStore store = new GameStore();
+    Game game1 = store.publishGame("Гарри Поттер", "RPG");
+    Game game2 = store.publishGame("Танки", "Стратегии");
+    Game game3 = store.publishGame("Нетология Баттл Онлайн", "Аркады");
+    Game game4 = store.publishGame("Симсы", "Симулятор");
+    Game game5 = store.publishGame("Контра", "Шутер");
 
     @Test
     public void shouldAddGame() {
@@ -13,6 +19,8 @@ public class GameStoreTest {
 
         assertTrue(store.containsGame(game));
     }
+
+
 
     // другие ваши тесты
 }
